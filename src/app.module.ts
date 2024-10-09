b5f9353948c72f9ea8ee2cc98e859aad6e577ea1
grubor-maja <maja.grubor@outlook.com>
2024-10-09 16:40:50 +0200

    add auth route for signup with validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
